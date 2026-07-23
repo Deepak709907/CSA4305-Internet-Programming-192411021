f1f16e407ebcfbf13fb2af3f14376a2f1be673b6
Deepak709907 <deepakplays77@gmail.com>
2026-07-23 10:24:32 +0530

    Add files via upload

---
+++ Code/EXP-8/js/script.js
@@ -0,0 +1,59 @@
+function login()
+{
+    alert("Login Successful!");
+}
+
+function registerUser()
+{
+    alert("Registration Successful!");
+}
+
+function searchBook()
+{
+    alert("Book Search Completed!");
+}
+
+function addBook()
+{
+    alert("Book Added Successfully!");
+}
+
+function updateBook()
+{
+    alert("Book Updated Successfully!");
+}
+
+function deleteBook()
+{
+    alert("Book Deleted Successfully!");
+}
+
+function issueBook()
+{
+    alert("Book Issued Successfully!");
+}
+
+function returnBook()
+{
+    alert("Book Returned Successfully!");
+}
+
+function viewProfile()
+{
+    alert("User Profile");
+}
+
+function downloadMaterial()
+{
+    alert("Download Started!");
+}
+
+function checkFine()
+{
+    alert("Fine Details Displayed!");
+}
+
+function sendMessage()
+{
+    alert("Message Sent Successfully!");
+}
